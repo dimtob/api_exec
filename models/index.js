5b94075a232032f21d9e63042981e7611c48286d
dimtob <dimitris_tob@hotmail.com>
2019-04-15 22:25:28 +0000

    final22

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose=require("mongoose")
-mongoose.connect("mongodb://localhost/apidb", { useNewUrlParser: true });
-
+//mongoose.connect("mongodb://localhost/apidb", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://todoDB:Skyvalley!@cluster0-pqbnr.mongodb.net/test?retryWrites=true", { useNewUrlParser: true });
 mongoose.Promise=Promise;
 
 module.exports.Todo = require("./todo");
+
