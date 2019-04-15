52122aa74d6b7d691d9d08aa039c11fe41accfe0
dimtob <dimitris_tob@hotmail.com>
2019-04-15 23:14:35 +0000

    atalast

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose=require("mongoose")
 //mongoose.connect("mongodb://localhost/apidb", { useNewUrlParser: true });
-mongoose.connect("mongodb+srv://todoDB:Skyvalley!@cluster0-pqbnr.mongodb.net/test?retryWrites=true", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://dimtob:dimtob@cluster0-pam4t.mongodb.net/test?retryWrites=true", { useNewUrlParser: true });
 mongoose.Promise=Promise;
 
 module.exports.Todo = require("./todo");
